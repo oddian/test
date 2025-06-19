5a7509076dc3fdcfc7b9549bef3940f3d8e96d93
oddian <oddian@gmail.com>
2025-06-19 11:49:31 +1000

    Merge work branch changes

--- README.md
+++ README.md
@@ -11,3 +11,20 @@ python3 -m http.server 8000
 ```
 
 Then open `http://localhost:8000` in a browser that supports PWAs.
+
+## Usage
+
+- Click **Add Recipe** to open a form for entering a new recipe.
+- Fill in the name and type fields and click **Save** to store it in IndexedDB.
+- Use the trash can icon next to a recipe to delete it from the list and IndexedDB.
+
+
+## Testing
+
+Run `npm install` to install development dependencies, then execute:
+
+```bash
+npm test
+```
+
+This runs the Jest test suite using a fake IndexedDB implementation.
---
+++ db.js
@@ -0,0 +1,67 @@
+const DB_NAME = 'recipes-db';
+const STORE_NAME = 'recipes';
+const DB_VERSION = 1;
+
+function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    request.onupgradeneeded = event => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+      }
+    };
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+async function getRecipes() {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, 'readonly');
+    const store = tx.objectStore(STORE_NAME);
+    const request = store.getAll();
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+async function addRecipe(recipe) {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    const request = store.add(recipe);
+    request.onsuccess = () => resolve();
+    request.onerror = () => reject(request.error);
+  });
+}
+
+async function deleteRecipe(id) {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    const request = store.delete(id);
+    request.onsuccess = () => resolve();
+    request.onerror = () => reject(request.error);
+  });
+}
+
+async function seedRecipes(recipes) {
+  const db = await openDB();
+  const tx = db.transaction(STORE_NAME, 'readwrite');
+  const store = tx.objectStore(STORE_NAME);
+  for (const r of recipes) {
+    store.add(r);
+  }
+  return new Promise((resolve, reject) => {
+    tx.oncomplete = () => resolve();
+    tx.onerror = () => reject(tx.error);
+  });
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = { openDB, getRecipes, addRecipe, deleteRecipe, seedRecipes };
+}
